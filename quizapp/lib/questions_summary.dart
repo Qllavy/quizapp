d95a7ec331f0fe0c8c3aa74c6ecbed7609113ed2
Qllavy <h-olech@wp.pl>
2025-07-24 09:09:43 +0200

    Styling the summary screen

--- quizapp/lib/questions_summary.dart
+++ quizapp/lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -6,29 +7,75 @@ class QuestionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     final isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
 
     return SizedBox(
-      height: isLandscape ? 120 : 400,
+      height: isLandscape ? 200 : 400,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            return Row(
-              children: [
-                Text(((data['question_index'] as int) + 1).toString()),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      SizedBox(height: 5),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
-                    ],
+            final questionIndex = (data['question_index'] as int) + 1;
+            final question = data['question'] as String;
+            final userAnswer = data['user_answer'] as String;
+            final correctAnswer = data['correct_answer'] as String;
+            final isCorrect = userAnswer == correctAnswer;
+
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundColor: isCorrect ? Colors.green : Colors.red,
+                    child: Text(
+                      questionIndex.toString(),
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          question,
+                          style: GoogleFonts.lato(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'Twoja odpowiedź: $userAnswer',
+                          style: GoogleFonts.lato(
+                            fontSize: 14,
+                            color: Colors.orange,
+                          ),
+                        ),
+                        Text(
+                          'Poprawna odpowiedź: $correctAnswer',
+                          style: GoogleFonts.lato(
+                            fontSize: 14,
+                            color: Colors.lightGreenAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             );
           }).toList(),
         ),
